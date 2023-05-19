1777d8b350eb88a7a89f9c6f9219bbae571f2338
javierbarrera574 <82546878+javierbarrera574@users.noreply.github.com>
2023-05-18 22:17:18 -0300

    Add files via upload

---
+++ EjercicioEnSwift.swift
@@ -0,0 +1,14 @@
+//Realizar una lista de supermercado con al menos 10 productos y mostrarlos por consola
+
+
+
+var listaSupermercado = ["Manzanas", "Leche", "Arroz", "Pan", "Huevos", "Queso", "Pollo", "Pasta", "Aceite", "Café"]
+
+listaSupermercado += ["Tomates", "Carne", "Azúcar", "Papel higiénico"]
+
+// Mostrar la lista de supermercado por consola
+print("Lista de Supermercado:")
+for producto in listaSupermercado 
+{
+    print("- \(producto)")
+}
